d7882076f8d1c927dee98331d26b49d565704db4
Marcus Oskarsson <oscarsso@esrf.fr>
2024-06-07 13:49:21 +0200

    [AbstractNState] - Fixed vlaue_to_enum

--- mxcubecore/HardwareObjects/abstract/AbstractNState.py
+++ mxcubecore/HardwareObjects/abstract/AbstractNState.py
@@ -87,17 +87,18 @@ class AbstractNState(AbstractActuator):
         except (ValueError, TypeError):
             pass
 
-    def value_to_enum(self, value):
+    def value_to_enum(self, value, idx=0):
         """Tranform a value to Enum
         Args:
            value(str, int, float, tuple): value
+           idx(int): Index in the value, if tuple
         Returns:
             (Enum): Enum member, corresponding to the value or UNKNOWN.
         """
         for enum_var in self.VALUES.__members__.values():
             if value == enum_var.value:
                 return enum_var
-            if isinstance(enum_var.value, tuple) and value == enum_var.value[0]:
+            if isinstance(enum_var.value, tuple) and value == enum_var.value[idx]:
                 return enum_var
 
         return self.VALUES.UNKNOWN
