a060469abc3382e588a66a3f9ff97b95ac9b2109
walesch-yan <yan.walesch@esrf.fr>
2024-10-07 15:21:21 +0200

    Switch to newest Argus grpc protocol syntax

--- mxcubecore/HardwareObjects/Argus.py
+++ mxcubecore/HardwareObjects/Argus.py
@@ -1,10 +1,9 @@
 import grpc
 import gevent
-from google.protobuf.json_format import MessageToDict
 from typing import List
-
 import argussight.grpc.argus_service_pb2 as pb2
 import argussight.grpc.argus_service_pb2_grpc as pb2_grpc
+from argussight.grpc.helper_functions import pack_to_any, unpack_from_any
 
 from mxcubecore.BaseHardwareObjects import HardwareObject
 
@@ -33,10 +32,20 @@ class Argus(HardwareObject):
             if response.status == "success":
                 if self.last_response == {} or self.last_response["status"] == "error":
                     self.last_response = {}
-                response_dict = MessageToDict(response)
+                # Unpack the message and convert it to a dict
+                running_processes = {}
+                for key, process in response.running_processes.items():
+                    settings = {}
+                    for setting, value in process.settings.items():
+                        settings[setting] = unpack_from_any(value)
+                    running_processes[key] = {
+                        "type": process.type,
+                        "commands": list(process.commands),
+                        "settings": settings,
+                    }
                 return (
-                    response_dict["runningProcesses"],
-                    response_dict["availableProcessTypes"],
+                    running_processes,
+                    list(response.available_process_types),
                 )
         except Exception as e:
             self.server_communication_error = True
@@ -67,10 +76,9 @@ class Argus(HardwareObject):
                     if current_running[process]["type"] in classes:
                         self.closable_running = True
                         break
-
                 self.emit("processesChanged")
                 self.emit("lastResponseChanged")
-            gevent.sleep(2)
+            gevent.sleep(5)
 
     def get_processes(self) -> dict:
         return {
@@ -89,25 +97,20 @@ class Argus(HardwareObject):
         )
         self.emit_last_response_change(response.status, response.error_message)
 
-    def start_process(self, name: str, type: str, **args):
+    def start_process(self, name: str, type: str):
         print(f"Sending start process request for {name}")
-        process = pb2.ProcessInfo(
-            name=name,
-            type=type,
-            args=[arg for arg in args["args"]],
+        response = self.stub.StartProcesses(
+            pb2.StartProcessesRequest(name=name, type=type)
         )
-        response = self.stub.StartProcesses(pb2.StartProcessesRequest(process=process))
         self.emit_last_response_change(response.status, response.error_message)
         if response.stream_id:
             self.new_stream(name, response.stream_id)
 
-    def manage_process(self, name: str, command: str, wait_time: int, **args):
+    def manage_process(self, name: str, command: str):
         print(f"Sending manage request for command {command} of process {name}")
         request = pb2.ManageProcessesRequest(
             name=name,
-            order=command,
-            wait_time=wait_time,
-            args=[arg for arg in args["args"]],
+            command=command,
         )
         response = self.stub.ManageProcesses(request)
         self.emit_last_response_change(response.status, response.error_message)
@@ -130,3 +133,11 @@ class Argus(HardwareObject):
 
     def get_streams(self):
         return self.streams
+
+    def change_settings(self, name: str, settings: dict) -> None:
+        converted_settings = {}
+        for key, setting in settings.items():
+            converted_settings[key] = pack_to_any(setting)
+        request = pb2.ChangeSettingsRequest(name=name, settings=converted_settings)
+        response = self.stub.ChangeSettings(request)
+        self.emit_last_response_change(response.status, response.error_message)
