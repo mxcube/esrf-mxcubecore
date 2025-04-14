099ddba6638edc5ae66e0f2c9e50df615084dfa3
Generic Bliss Account For Control Software <blissadm@id30bcontrol2.esrf.fr>
2025-04-14 14:29:36 +0200

    [TMP] - to be rebeased

--- mxcubecore/BaseHardwareObjects.py
+++ mxcubecore/BaseHardwareObjects.py
@@ -431,11 +431,11 @@ class HardwareObjectNode:
             ) = self.__references.pop()
 
             hw_object = get_hardware_repository().get_hardware_object(reference)
-            warnings.warn(
-                "Deprecated: Hardware object '{}' with role '{}' is a reference".format(
-                    name, role
-                )
-            )
+            #warnings.warn(
+            #    "Deprecated: Hardware object '{}' with role '{}' is a reference".format(
+            #        name, role
+            #    )
+            #)
 
             if hw_object is not None:
                 self._objects_by_role[role] = hw_object
@@ -522,11 +522,6 @@ class HardwareObjectNode:
             Union[HardwareObject, None]: Hardware object.
         """
         role = str(role).lower()
-        warnings.warn(
-            f"{self.__class__.__name__}.get_object_by_role is deprecated. "
-            f"Use attribute '{role}' to access this object."
-        )
-
         #
         # A hack to emulate get_object_by_role() for objects loaded from YAML config files.
         #
