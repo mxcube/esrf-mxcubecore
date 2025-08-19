5e484b3e0f14cbd9f9778c4eb15ee640a9bcaee3
Generic Bliss Account For Control Software <blissadm@id30bcontrol2.esrf.fr>
2025-08-19 13:32:58 +0200

    [Centring] - Making centring more robust

--- mxcubecore/queue_entry/base_queue_entry.py
+++ mxcubecore/queue_entry/base_queue_entry.py
@@ -871,8 +871,7 @@ def mount_sample(data_model, centring_done_cb, async_result):
                 elif centring_method == CENTRING_METHOD.LOOP:
                     sview.start_auto_centring()
                     log.warning(
-                        "Centring in progress. Please save"
-                        + " the suggested centring or re-center"
+                        "Centring sample, please wait"
                     )
                 elif centring_method == CENTRING_METHOD.FULLY_AUTOMATIC:
                     log.info("Centring sample, please wait.")
