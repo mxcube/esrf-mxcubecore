66b7d3ebfac9dc4a3010ca67fa688a4bc5f8192f
Marcus Oskarsson <oscarsso@esrf.fr>
2025-12-08 13:52:35 +0100

    Trigger sample_info_changed when the state of a sample changes

--- mxcubecore/HardwareObjects/abstract/sample_changer/Component.py
+++ mxcubecore/HardwareObjects/abstract/sample_changer/Component.py
@@ -14,6 +14,11 @@ class Component(object):
         self.dirty = False
         self._leaf = False
         self._name = ""
+        self.state = ""
+        self.puck_barcode = ""
+        self.sample_barcode = ""
+        self.puck_type = ""
+        
 
     #########################           PUBLIC           #########################
 
