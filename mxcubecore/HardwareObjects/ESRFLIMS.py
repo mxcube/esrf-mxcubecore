4a51fffc3f0a69dbd63f904bdf20f62f86732927
Marcus Oskarsson <oscarsso@esrf.fr>
2026-07-07 20:02:19 +0200

    Adding set_active_user on ESRFLIMS

--- mxcubecore/HardwareObjects/ESRFLIMS.py
+++ mxcubecore/HardwareObjects/ESRFLIMS.py
@@ -171,6 +171,9 @@ class ESRFLIMS(AbstractLims):
 
     def is_session_already_active(self, session_id: str) -> bool:
         return self.drac.is_session_already_active(session_id)
+    
+    def set_active_user(self, username: str):
+        self.drac.set_active_user(username)
 
     def set_active_session_by_id(
         self, session_id: str, username: str
